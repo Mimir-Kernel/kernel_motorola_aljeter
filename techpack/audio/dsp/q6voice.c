wiktorek140/msm-4.9-new

techpack: audio: q6voice: Force AVCS CVP version 1 on legacy ADSP
Force the AVCS version to CVP_VERSION_1 for all of the AVCS
services on all legacy ADSP firmwares: these ones cannot execute
the Q6 Service Version command on their Q6 DSP because of older
firmware not supporting it.
We know that for the old firmware the version is always 1, hence
forcing it is totally safe.

based on: https://github.com/zexceed12300/android_kernel_xiaomi_rosy-4.9/commit/0ea7419d413e4a5a76e1090ed5e9a3f4fa5e4c1f
Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
Change-Id: I49484c7fa329cb793314a04b3feded3841702dbf

techpack/audio/dsp/q6voice.c
@@ -38,6 +38,7 @@
 #define NUM_CHANNELS_STEREO 2
 #define NUM_CHANNELS_THREE 3
 #define NUM_CHANNELS_QUAD 4
+#define CVP_VERSION_1 1
 #define CVP_VERSION_2 2
 #define GAIN_Q14_FORMAT(a) (a << 14)
 
@@ -4356,6 +4357,11 @@ static int voice_get_avcs_version_per_service(uint32_t service_id)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MONTANA_DTB
+	common.is_avcs_version_queried = true;
+	return CVP_VERSION_1;
+#else
+
 	ver_size = sizeof(struct avcs_get_fwk_version) +
 		   sizeof(struct avs_svc_api_info);
 	ver_info = kzalloc(ver_size, GFP_KERNEL);
@@ -4371,6 +4377,7 @@ static int voice_get_avcs_version_per_service(uint32_t service_id)
 done:
 	kfree(ver_info);
 	return ret;
+#endif
 }
 
 static void voice_mic_break_work_fn(struct work_struct *work)

Mimir-Kernel/kernel_motorola_aljeter

techpack/audio/dsp/q6voice.c
@@ -38,6 +38,7 @@
 #define NUM_CHANNELS_STEREO 2
 #define NUM_CHANNELS_THREE 3
 #define NUM_CHANNELS_QUAD 4
+#define CVP_VERSION_1 1
 #define CVP_VERSION_2 2
 #define GAIN_Q14_FORMAT(a) (a << 14)
 
@@ -4355,6 +4356,11 @@ static int voice_get_avcs_version_per_service(uint32_t service_id)
 		       AVCS_SERVICE_ID_ALL);
 		return -EINVAL;
 	}
+
+#ifdef CONFIG_SND_LEGACY
+	common.is_avcs_version_queried = true;
+	return CVP_VERSION_1;
+#endif
 
 	ver_size = sizeof(struct avcs_get_fwk_version) +
 		   sizeof(struct avs_svc_api_info);